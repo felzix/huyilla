732890b038b1ae7e0a236c666287570b17be8447
robert <robert@augur.net>
2019-04-09 22:15:52 -0700

    auth test uses goblin

--- engine/engine/auth_test.go
+++ engine/engine/auth_test.go
@@ -1,183 +1,151 @@
 package engine
 
 import (
+	"fmt"
+	. "github.com/felzix/goblin"
+	"github.com/felzix/huyilla/types"
+	uuid "github.com/satori/go.uuid"
 	"testing"
 	"time"
 )
 
-func TestHuyilla_SignUp(t *testing.T) {
-	h := &Engine{}
-	h.Init("/tmp/huyilla")
-	defer h.World.WipeDatabase()
+func Test(t *testing.T) {
+	g := Goblin(t)
+	g.Describe("Auth", func() {
+		var h *Engine
 
-	if err := h.SignUp("felzix", "PASS"); err != nil {
-		t.Fatal(err)
-	}
+		g.BeforeEach(func() {
+			unique, err := uuid.NewV4()
+			if err != nil {
+				t.Fatal(err)
+			}
 
-	player, err := h.World.Player("felzix")
-	if err != nil {
-		t.Fatalf("Error: %v", err)
-	}
+			h = &Engine{}
+			if err := h.Init("/tmp/savedir-huyilla-" + unique.String()); err != nil {
+				t.Fatal(err)
+			}
+		})
 
-	entity, err := h.World.Entity(player.EntityId)
-	if entity == nil {
-		t.Fatalf("Entity %d should exist but doesn't", player.EntityId)
-	} else if err != nil {
-		t.Fatal(err)
-	}
+		g.AfterEach(func() {
+			if h == nil || h.World == nil {
+				return
+			}
+			if err := h.World.WipeDatabase(); err != nil {
+				t.Fatal(err)
+			}
+		})
 
-	if player.Name != entity.PlayerName {
-		t.Errorf(
-			`Player's entity has wrong name: player="%v", entity="%v"`,
-			player.Name, entity.PlayerName)
-	}
-}
+		g.It("signs up", func() {
+			err := h.SignUp("felzix", "PASS")
+			g.Assert(err).Equal(nil)
 
-func TestHuyilla_Login(t *testing.T) {
-	h := &Engine{}
-	h.Init("/tmp/huyilla")
-	defer h.World.WipeDatabase()
+			player, err := h.World.Player("felzix")
+			g.Assert(err).Equal(nil)
+			g.Assert(player).NotEqual(nil)
 
-	if err := h.SignUp("allomance", "PASS"); err != nil {
-		t.Fatal(err)
-	}
+			entity, err := h.World.Entity(player.EntityId)
+			g.Assert(err).Equal(nil)
+			g.Assert(entity).NotEqual(nil)
 
-	token, err := h.LogIn("allomance", "PASS")
-	if err != nil {
-		t.Fatal(err)
-	}
+			g.Assert(player.Name).Equal(entity.PlayerName)
+		})
 
-	if len(token) < 100 || token[0] != 'e' {
-		t.Errorf(`Bad token. Token="%s"`, token)
-	}
+		g.It("logs in", func() {
+			err := h.SignUp("felzix", "PASS")
+			g.Assert(err).Equal(nil)
 
-	player, err := h.World.Player("allomance")
-	if err != nil {
-		t.Fatalf("Error: %v", err)
-	}
+			token, err := h.LogIn("felzix", "PASS")
+			g.Assert(err).Equal(nil)
+			g.Assert(len(token) > 100).IsTrue("token is not set or is set incorrectly")
+			g.Assert(token[0]).Equal(byte('e'))
 
-	if len(player.Token) == 0 {
-		t.Error(`Player should have been given a token`)
-	}
+			player, err := h.World.Player("felzix")
+			g.Assert(err).Equal(nil)
+			g.Assert(player).NotEqual(nil)
+			g.Assert(player.Token).Equal(token)
 
-	entity, err := h.World.Entity(player.EntityId)
-	if entity == nil {
-		t.Fatalf("Entity %d should exist but doesn't", player.EntityId)
-	} else if err != nil {
-		t.Fatal(err)
-	}
+			entity, err := h.World.Entity(player.EntityId)
+			g.Assert(err).Equal(nil)
+			g.Assert(entity).NotEqual(nil)
 
-	chunk, err := h.World.Chunk(entity.Location.Chunk)
-	if err != nil {
-		t.Fatal(err)
-	}
+			chunk, err := h.World.Chunk(entity.Location.Chunk)
+			g.Assert(err).Equal(nil)
 
-	entityIsPresent := false
-	for i := 0; i < len(chunk.Entities); i++ {
-		entity := chunk.Entities[i]
-		if entity == player.EntityId {
-			entityIsPresent = true
-		}
-	}
-	if !entityIsPresent {
-		t.Errorf(`Expected entity at chunk (%d,%d,%d) but it was not there`,
-			entity.Location.Chunk.X,
-			entity.Location.Chunk.Y,
-			entity.Location.Chunk.Z)
-	}
-}
+			entityIsPresent := false
+			for i := 0; i < len(chunk.Entities); i++ {
+				entity := chunk.Entities[i]
+				if entity == player.EntityId {
+					entityIsPresent = true
+				}
+			}
+			g.Assert(entityIsPresent).IsTrue(fmt.Sprintf(
+				`Expected entity at chunk (%d,%d,%d) but it was not there`,
+				entity.Location.Chunk.X,
+				entity.Location.Chunk.Y,
+				entity.Location.Chunk.Z))
+		})
 
-func TestHuyilla_LoginNegative(t *testing.T) {
-	h := &Engine{}
-	h.Init("/tmp/huyilla")
-	defer h.World.WipeDatabase()
+		g.It("logs out", func() {
+			err := h.SignUp("felzix", "PASS")
+			g.Assert(err).Equal(nil)
 
-	_, err := h.LogIn("felzix", "PASS")
-	if err == nil {
-		t.Fatal("Logging in before signup should throw an error but didn't")
-	} else if err.Error() != `No such player "felzix"` {
-		t.Errorf(`Wrong error. Got "%v"`, err)
-	}
-}
+			_, err = h.LogIn("felzix", "PASS")
+			g.Assert(err).Equal(nil)
 
-func TestHuyilla_Logout(t *testing.T) {
-	h := &Engine{}
-	h.Init("/tmp/huyilla")
-	defer h.World.WipeDatabase()
+			err = h.LogOut("felzix")
+			g.Assert(err).Equal(nil)
 
-	if err := h.SignUp("allomance", "PASS"); err != nil {
-		t.Fatal(err)
-	}
+			player, err := h.World.Player("felzix")
+			g.Assert(err).Equal(nil)
+			g.Assert(player).NotEqual(nil)
+			g.Assert(player.Token).Equal("")
 
-	_, err := h.LogIn("allomance", "PASS")
-	if err != nil {
-		t.Fatal(err)
-	}
+			entity, err := h.World.Entity(player.EntityId)
+			g.Assert(err).Equal(nil)
+			g.Assert(entity).NotEqual(nil)
 
-	if err := h.LogOut("allomance"); err != nil {
-		t.Fatal(err)
-	}
+			chunk, err := h.World.Chunk(entity.Location.Chunk)
+			g.Assert(err).Equal(nil)
 
-	player, err := h.World.Player("allomance")
-	if err != nil {
-		t.Fatalf("Error: %v", err)
-	}
+			entityIsPresent := false
+			for i := 0; i < len(chunk.Entities); i++ {
+				entity := chunk.Entities[i]
+				if entity == player.EntityId {
+					entityIsPresent = true
+				}
+			}
+			g.Assert(entityIsPresent).IsFalse(fmt.Sprintf(
+				`Expected entity to NOT be at chunk (%d,%d,%d) but it was there.`,
+				entity.Location.Chunk.X,
+				entity.Location.Chunk.Y,
+				entity.Location.Chunk.Z))
+		})
 
-	if len(player.Token) > 0 {
-		t.Error(`Player should not have a token`)
-	}
+		g.It("fails to log in", func() {
+			token, err := h.LogIn("felzix", "PASS")
+			g.Assert(token).Equal("")
+			g.Assert(err).NotEqual(nil)
+			g.Assert(err.Error()).Equal(`No such player "felzix"`)
 
-	entity, err := h.World.Entity(player.EntityId)
-	if entity == nil {
-		t.Fatalf("Entity %d should exist but doesn't", player.EntityId)
-	} else if err != nil {
-		t.Fatal(err)
-	}
+			player, err := h.World.Player("felzix")
+			g.Assert(err).Equal(nil)
+			g.Assert(player).Equal((*types.Player)(nil))
+		})
 
-	chunk, err := h.World.Chunk(entity.Location.Chunk)
-	if err != nil {
-		t.Fatal(err)
-	}
+		g.It("token test", func() {
+			SECRET := []byte("secret")
+			NAME := "camian"
+			EXPIRY := time.Now().Add(time.Hour * 24).Unix()
 
-	entityIsPresent := false
-	for i := 0; i < len(chunk.Entities); i++ {
-		entity := chunk.Entities[i]
-		if entity == player.EntityId {
-			entityIsPresent = true
-		}
-	}
-	if entityIsPresent {
-		t.Errorf(`Unexpected entity at chunk (%d,%d,%d). It should have been removed on logout`,
-			entity.Location.Chunk.X,
-			entity.Location.Chunk.Y,
-			entity.Location.Chunk.Z)
-	}
-}
+			token, err := makeToken(SECRET, NAME, EXPIRY)
+			g.Assert(err).Equal(nil)
 
-func TestHuyilla_token(t *testing.T) {
-	SECRET := []byte("secret")
-	NAME := "camian"
-	EXPIRY := time.Now().Add(time.Hour * 24).Unix()
+			name, tokenId, expiry, err := readToken(SECRET, token)
+			g.Assert(err).Equal(nil)
 
-	token, err := makeToken(SECRET, NAME, EXPIRY)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	name, tokenId, expiry, err := readToken(SECRET, token)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if name != NAME {
-		t.Errorf(`Name mismatch: "%s" != "%s"`, name, NAME)
-	}
-
-	if expiry != EXPIRY {
-		t.Errorf(`Expiry mismatch: "%d" != "%d"`, expiry, EXPIRY)
-	}
-
-	if len(tokenId) == 0 {
-		t.Errorf("Expected a token id but it's empty")
-	}
+			g.Assert(name).Equal(NAME)
+			g.Assert(expiry).Equal(EXPIRY)
+			g.Assert(len(tokenId) > 0).IsTrue("Expected a token id but it's empty")
+		})
+	})
 }
