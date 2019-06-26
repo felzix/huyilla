d04eca91f8360c63bae9413494274116d2106c7f
robert <robert@augur.net>
2019-06-25 20:21:38 -0700

    engine refactor

--- engine/engine/engine.go
+++ engine/engine/engine.go
@@ -47,52 +47,41 @@ func (engine *Engine) Tick() error {
 		return errors.Wrap(err, "get active players")
 	}
 
-	activeChunks := make(map[types.ComparablePoint]*types.Chunk, len(players)*C.ACTIVE_CHUNK_CUBE)
+	vitalizedVoxels := make([]types.Point, C.PASSIVE_VITALITY)
+	for i := 0; i < C.PASSIVE_VITALITY; i++ {
+		vitalizedVoxels[i] = *types.RandomPoint(C.CHUNK_SIZE)
+	}
+
 	for i := 0; i < len(players); i++ {
 		player := players[i]
 		loc := player.Entity.Location.Chunk
 		for x := loc.X - C.ACTIVE_CHUNK_RADIUS; x < 1+loc.X+C.ACTIVE_CHUNK_RADIUS; x++ {
 			for y := loc.Y - C.ACTIVE_CHUNK_RADIUS; y < 1+loc.Y+C.ACTIVE_CHUNK_RADIUS; y++ {
 				for z := loc.Z - C.ACTIVE_CHUNK_RADIUS; z < 1+loc.Z+C.ACTIVE_CHUNK_RADIUS; z++ {
-					point := types.NewPoint(x, y, z)
-					if chunk, err := engine.World.Chunk(point); err == nil {
-						activeChunks[*types.NewComparablePoint(point)] = chunk
+					p := types.NewPoint(x, y, z)
+					if chunk, err := engine.World.Chunk(p); err == nil {
+						// voxel physics
+						for i := 0; i < C.PASSIVE_VITALITY; i++ {
+							point := types.AbsolutePoint{Chunk: p, Voxel: &vitalizedVoxels[i]}
+							if err := engine.voxelPhysics(chunk, &point); err != nil {
+								return errors.Wrap(err, "voxel physics of random voxels")
+							}
+						}
+
+						// entity physics
+						for i := 0; i < len(chunk.Entities); i++ {
+							entity, err := engine.World.Entity(chunk.Entities[i])
+							if err != nil {
+								return err
+							}
+							if err := engine.entityPhysics(chunk, entity); err != nil {
+								return errors.Wrap(err, "entity physics")
+							}
+						}
 					} else {
 						return errors.Wrap(err, "failed to get/gen chunk")
 					}
 				}
-			}
-		}
-	}
-
-	vitalizedVoxels := make([]types.Point, C.PASSIVE_VITALITY)
-	for i := 0; i < C.PASSIVE_VITALITY; i++ {
-		vitalizedVoxels[i] = *types.RandomPoint(C.CHUNK_SIZE)
-	}
-
-	for cp, chunk := range activeChunks {
-		p := types.NewPoint(cp.X, cp.Y, cp.Z)
-		for i := 0; i < len(chunk.ActiveVoxels); i++ {
-			point := types.AbsolutePoint{Chunk: p, Voxel: chunk.ActiveVoxels[i]}
-			if err := engine.voxelPhysics(chunk, &point); err != nil {
-				return errors.Wrap(err, "voxel physics of active voxels")
-			}
-		}
-
-		for i := 0; i < C.PASSIVE_VITALITY; i++ {
-			point := types.AbsolutePoint{Chunk: p, Voxel: &vitalizedVoxels[i]}
-			if err := engine.voxelPhysics(chunk, &point); err != nil {
-				return errors.Wrap(err, "voxel physics of random voxels")
-			}
-		}
-
-		for i := 0; i < len(chunk.Entities); i++ {
-			entity, err := engine.World.Entity(chunk.Entities[i])
-			if err != nil {
-				return err
-			}
-			if err := engine.entityPhysics(chunk, entity); err != nil {
-				return errors.Wrap(err, "entity physics")
 			}
 		}
 	}
